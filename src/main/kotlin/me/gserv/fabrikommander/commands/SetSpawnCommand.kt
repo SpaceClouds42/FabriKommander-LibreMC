bf59f5d24e28a16280cb1cf9a599b0a8afd85d2d
SpaceClouds42 <spaceclouds42@gmail.com>
2020-12-22 12:36:31 -0500

    Add /spawn and /setspawn. uses spawn.yaml config to store spawn position.

---
+++ src/main/kotlin/me/gserv/fabrikommander/commands/SetSpawnCommand.kt
@@ -0,0 +1,44 @@
+package me.gserv.fabrikommander.commands
+
+import me.gserv.fabrikommander.data.SpawnDataManager
+import me.gserv.fabrikommander.data.spec.Spawn
+import me.gserv.fabrikommander.data.spec.Pos
+import me.gserv.fabrikommander.utils.*
+import net.minecraft.server.command.CommandManager
+
+class SetSpawnCommand(val dispatcher: Dispatcher) {
+    fun register() {
+        dispatcher.register(
+            CommandManager.literal("setspawn")
+                .executes { setSpawnCommand(it) }
+                .requires { it.hasPermissionLevel(2) }
+        )
+    }
+
+    fun setSpawnCommand(context: Context): Int {
+        val player = context.source.player
+
+        val spawn = Spawn(
+            pos = Pos(
+                x = player.x,
+                y = player.y,
+                z = player.z,
+                yaw = player.yaw,
+                pitch = player.pitch,
+                world = player.world.registryKey.value
+            ),
+        )
+
+        SpawnDataManager.setSpawn(spawn)
+
+        context.source.sendFeedback(
+            green("Spawn created at ") +
+                    aqua("[${player.x.toInt()}, ${player.y.toInt()}, ${player.z.toInt()}]") +
+                    green(" in the ") +
+                    aqua(identifierToWorldName(player.world.registryKey.value)),
+            true
+        )
+
+        return 1
+    }
+}
---
+++ src/main/kotlin/me/gserv/fabrikommander/data/SpawnDataManager.kt
@@ -0,0 +1,80 @@
+package me.gserv.fabrikommander.data
+
+import com.charleskorn.kaml.Yaml
+import me.gserv.fabrikommander.data.spec.Player
+import me.gserv.fabrikommander.data.spec.Pos
+import me.gserv.fabrikommander.data.spec.Spawn
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
+import net.minecraft.util.Identifier
+import net.minecraft.util.WorldSavePath
+import org.apache.logging.log4j.LogManager
+import java.nio.file.Path
+import kotlin.NoSuchElementException
+
+object SpawnDataManager {
+    private val logger = LogManager.getLogger(this::class.java)
+
+    private lateinit var dataDir: Path
+
+    fun setup() {
+        ServerLifecycleEvents.SERVER_STARTING.register {
+            dataDir = it.getSavePath(WorldSavePath.ROOT).resolve("FabriKommander")
+
+            dataDir.toFile().mkdir()
+
+            logger.info("Data directory: $dataDir")
+        }
+    }
+
+    fun loadData(): Spawn {
+        val spawnFile = dataDir.resolve("spawn.yaml").toFile()
+
+        if (!spawnFile.exists()) {
+            spawnFile.createNewFile()
+
+            val data = Spawn(pos = Pos(
+                0.0,
+                75.0,
+                0.0,
+
+                0F,
+                0F,
+
+                Identifier("minecraft:overworld")
+            ))
+
+            spawnFile.writeText(
+                Yaml.default.encodeToString(Spawn.serializer(), data)
+            )
+
+            return data
+        }
+
+        val string = spawnFile.readText()
+        return Yaml.default.decodeFromString(Spawn.serializer(), string)
+    }
+
+    fun setSpawn(spawn: Spawn): Boolean? {
+        saveData(spawn)
+
+        return true
+    }
+
+
+    fun getSpawn(): Spawn {
+        return loadData()
+    }
+
+    fun saveData(spawn: Spawn) {
+        val spawnFile = dataDir.resolve("spawn.yaml").toFile()
+            ?: throw NoSuchElementException("No spawn config found")
+
+        if (!spawnFile.exists()) {
+            spawnFile.createNewFile()
+        }
+
+        spawnFile.writeText(
+            Yaml.default.encodeToString(Spawn.serializer(), spawn)
+        )
+    }
+}
