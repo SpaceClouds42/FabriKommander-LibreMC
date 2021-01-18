27028079bcef43d1b4e8cb23ba06aea3e8732a40
SpaceClouds42 <spaceclouds42@gmail.com>
2021-01-18 12:22:00 -0500

    fix /anvil

---
+++ src/main/java/me/gserv/fabrikommander/mixin/AnvilScreenHandlerMixin.java
@@ -0,0 +1,23 @@
+package me.gserv.fabrikommander.mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.screen.AnvilScreenHandler;
+import net.minecraft.screen.ForgingScreenHandler;
+import net.minecraft.screen.ScreenHandlerContext;
+import net.minecraft.screen.ScreenHandlerType;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(AnvilScreenHandler.class)
+public abstract class AnvilScreenHandlerMixin extends ForgingScreenHandler {
+    public AnvilScreenHandlerMixin(@Nullable ScreenHandlerType<?> type, int syncId, PlayerInventory playerInventory, ScreenHandlerContext context) {
+        super(type, syncId, playerInventory, context);
+    }
+
+    public void close(PlayerEntity player) {
+        super.close(player);
+        this.dropInventory(player, this.input);
+    }
+}
--- src/main/kotlin/me/gserv/fabrikommander/commands/donor/AnvilCommand.kt
+++ src/main/kotlin/me/gserv/fabrikommander/commands/donor/AnvilCommand.kt
@@ -4,12 +4,21 @@ import me.gserv.fabrikommander.extension.hasRankPermissionLevel
 import me.gserv.fabrikommander.utils.Context
 import me.gserv.fabrikommander.utils.Dispatcher
 import me.gserv.fabrikommander.utils.Node
+import net.minecraft.block.AbstractBlock
+import net.minecraft.block.AnvilBlock
+import net.minecraft.block.MapColor
+import net.minecraft.block.Material
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.entity.player.PlayerInventory
 import net.minecraft.screen.AnvilScreenHandler
 import net.minecraft.screen.ScreenHandlerContext
 import net.minecraft.screen.SimpleNamedScreenHandlerFactory
 import net.minecraft.server.command.CommandManager
+import net.minecraft.sound.BlockSoundGroup
 import net.minecraft.stat.Stats
 import net.minecraft.text.LiteralText
+import net.minecraft.text.TranslatableText
+import net.minecraft.util.math.BlockPos
 
 class AnvilCommand(val dispatcher: Dispatcher) {
     fun register() {
@@ -25,10 +34,27 @@ class AnvilCommand(val dispatcher: Dispatcher) {
 
     private fun anvilCommand(context: Context): Int {
         val player = context.source.player
+        /*val handledScreen =
+            SimpleNamedScreenHandlerFactory({ syncId: Int, playerInventory: PlayerInventory?, _: PlayerEntity? ->
+                AnvilScreenHandler(
+                    syncId,
+                    playerInventory,
+                    ScreenHandlerContext.create(player.world, BlockPos(player.x, player.y, player.z))
+                )
+            }, TranslatableText("container.repair"))
+        player.openHandledScreen(handledScreen)*/
+        /*val state = AnvilBlock(
+            AbstractBlock.Settings.of(Material.REPAIR_STATION, MapColor.IRON)
+                .requiresTool()
+                .strength(5.0f, 1200.0f)
+                .sounds(BlockSoundGroup.ANVIL)
+        ).defaultState
+
+        player.openHandledScreen(state.createScreenHandlerFactory(player.world, BlockPos(player.x, player.y, player.z)))*/
         player.openHandledScreen(
             SimpleNamedScreenHandlerFactory(
                 { syncId, inv, _ -> AnvilScreenHandler(syncId, inv, ScreenHandlerContext.EMPTY) },
-                LiteralText("Anvil")
+                TranslatableText("container.repair")
             )
         )
         player.incrementStat(Stats.INTERACT_WITH_ANVIL)
