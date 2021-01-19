4ab110d4764240a5aa4bcef87036f9e9fab84644
SpaceClouds42 <spaceclouds42@gmail.com>
2021-01-19 08:52:18 -0500

    add /bedhome

--- src/main/kotlin/me/gserv/fabrikommander/Common.kt
+++ src/main/kotlin/me/gserv/fabrikommander/Common.kt
@@ -37,6 +37,7 @@ object Common : ModInitializer {
 
         // Donor commands
         AnvilCommand(dispatcher).register()
+        BedHomeCommand(dispatcher).register()
         CraftCommand(dispatcher).register()
         EnderChestCommand(dispatcher).register()
         GlintCommand(dispatcher).register()
---
+++ src/main/kotlin/me/gserv/fabrikommander/commands/donor/BedHomeCommand.kt
@@ -0,0 +1,46 @@
+package me.gserv.fabrikommander.commands.donor
+
+import me.gserv.fabrikommander.data.PlayerDataManager
+import me.gserv.fabrikommander.data.spec.Pos
+import me.gserv.fabrikommander.extension.hasRankPermissionLevel
+import me.gserv.fabrikommander.utils.Context
+import me.gserv.fabrikommander.utils.Dispatcher
+import me.gserv.fabrikommander.utils.Node
+import me.gserv.fabrikommander.utils.red
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.server.command.CommandManager
+import net.minecraft.util.math.Vec3d
+
+class BedHomeCommand(val dispatcher: Dispatcher) {
+    fun register() {
+        val bedHomeNode: Node =
+            CommandManager
+                .literal("bedhome")
+                .requires { it.player.hasRankPermissionLevel("VIP") }
+                .executes { bedHomeCommand(it) }
+                .build()
+
+        dispatcher.root.addChild(bedHomeNode)
+    }
+
+    fun bedHomeCommand(context: Context): Int {
+        val player = context.source.player
+
+        val world = player.server.getWorld(player.spawnPointDimension)
+        val respawnBlockPos = player.spawnPointPosition
+        val spawnAngle = player.spawnAngle
+        val spawnPosSet = player.isSpawnPointSet
+        val alive = true
+        val optionalRespawnVec3d = PlayerEntity.findRespawnPosition(world, respawnBlockPos, spawnAngle, spawnPosSet, alive)
+        if (optionalRespawnVec3d.isEmpty) {
+            context.source.sendError(
+                red("Respawn position not found")
+            )
+            return 0
+        }
+        PlayerDataManager.setBackPos(player.uuid, Pos(player.x, player.y, player.z, player.yaw, player.pitch, player.world.registryKey.value))
+        val respawnVec3d = optionalRespawnVec3d.get()
+        player.teleport(world, respawnVec3d.x, respawnVec3d.y, respawnVec3d.z, player.yaw, player.pitch)
+        return 1
+    }
+}
