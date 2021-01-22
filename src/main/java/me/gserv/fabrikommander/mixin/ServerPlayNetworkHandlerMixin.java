058ed46069809cb8582abd965a22337f36fb61cc
SpaceClouds42 <spaceclouds42@gmail.com>
2021-01-22 06:09:57 -0500

    format join and leave messages

--- src/main/java/me/gserv/fabrikommander/mixin/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/me/gserv/fabrikommander/mixin/ServerPlayNetworkHandlerMixin.java
@@ -1,13 +1,20 @@
 package me.gserv.fabrikommander.mixin;
 
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
 import me.gserv.fabrikommander.data.PlayerDataManager;
 import net.minecraft.network.MessageType;
+import net.minecraft.network.Packet;
 import net.minecraft.network.packet.c2s.play.ChatMessageC2SPacket;
+import net.minecraft.network.packet.s2c.play.GameMessageS2CPacket;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Util;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -21,7 +28,7 @@ import static me.gserv.fabrikommander.utils.TextFormatterKt.formatChatMessage;
 import static me.gserv.fabrikommander.utils.TextKt.*;
 
 @Mixin(ServerPlayNetworkHandler.class)
-public class ServerPlayNetworkHandlerMixin {
+public abstract class ServerPlayNetworkHandlerMixin {
     @Shadow
     public ServerPlayerEntity player;
     @Shadow
@@ -57,16 +64,17 @@ public class ServerPlayNetworkHandlerMixin {
                     rawMessage
             );
             info.cancel();
+        // Log Private Messages
         } else if (packet.getChatMessage().startsWith("/msg ")) {
             int spaceAfterTargetPlayer = 5 + packet.getChatMessage().substring(5).indexOf(" ");
             String message = packet.getChatMessage().substring(spaceAfterTargetPlayer);
             String targetPlayer = packet.getChatMessage().substring(5, spaceAfterTargetPlayer);
-            log("chat.private", player.getEntityName() + " to " + targetPlayer + ":" + message);
+            log("chat.private", "[PM] " + player.getEntityName() + " to " + targetPlayer + ":" + message);
         } else if (packet.getChatMessage().startsWith("/tell ")) {
             int spaceAfterTargetPlayer = 6 + packet.getChatMessage().substring(6).indexOf(" ");
             String message = packet.getChatMessage().substring(spaceAfterTargetPlayer);
             String targetPlayer = packet.getChatMessage().substring(6, spaceAfterTargetPlayer);
-            log("chat.private", player.getEntityName() + " to " + targetPlayer + ":" + message);
+            log("chat.private", "[PM] " + player.getEntityName() + " to " + targetPlayer + ":" + message);
         } else if (packet.getChatMessage().startsWith("/w ")) {
             int spaceAfterTargetPlayer = 3 + packet.getChatMessage().substring(3).indexOf(" ");
             String message = packet.getChatMessage().substring(spaceAfterTargetPlayer);
@@ -84,6 +92,24 @@ public class ServerPlayNetworkHandlerMixin {
         log("chat.main", "[Chat] " + sender.getEntityName() + " > " + rawMessage);
     }
 
+    private void broadcastJoinMsg(MutableText message, ServerPlayerEntity sender) {
+        server.getPlayerManager().broadcastChatMessage(
+                message,
+                MessageType.SYSTEM,
+                sender.getUuid()
+        );
+        log("chat.join", "[+] " + sender.getEntityName());
+    }
+
+    private void broadcastLeaveMsg(MutableText message, ServerPlayerEntity sender) {
+        server.getPlayerManager().broadcastChatMessage(
+                message,
+                MessageType.SYSTEM,
+                sender.getUuid()
+        );
+        log("chat.leave", "[-] " + sender.getEntityName());
+    }
+
     private void broadcastStaffMsg(MutableText message, MinecraftServer server, String rawMessage, String sender) {
         server.getPlayerManager().getPlayerList().forEach(p -> {
             if (getStaffRanks().contains(PlayerDataManager.INSTANCE.getRank(p.getUuid()))) {
@@ -92,4 +118,93 @@ public class ServerPlayNetworkHandlerMixin {
         });
         log("chat.staff", "[Staff] " + sender + " > " + rawMessage);
     }
+
+    /**
+     * Catches join/leave messages and
+     * formats them
+     *
+     * @param packet
+     * @param listener
+     * @param ci
+     */
+    @Inject(method = "sendPacket(Lnet/minecraft/network/Packet;Lio/netty/util/concurrent/GenericFutureListener;)V", at = @At("HEAD"), cancellable = true)
+    private void fancyJoinLeaveMessages(Packet<?> packet, GenericFutureListener<? extends Future<? super Void>> listener, CallbackInfo ci) {
+        if (packet instanceof GameMessageS2CPacket && isJoinLeaveMessage((GameMessageS2CPacket) packet)) {
+            if (isJoinMessage((GameMessageS2CPacket) packet)) {
+                broadcastJoinMsg(
+                        reset("")
+                            .append(
+                                darkGray("[")
+                            )
+                            .append(
+                                darkGreen("+")
+                            ).append(
+                                darkGray("] ")
+                            ).append(
+                                (MutableText) ((TranslatableText) ((GameMessageS2CPacket) packet).getMessage()).getArgs()[0]
+                            )
+                        ,
+                        this.player
+                );
+            } else {
+                broadcastLeaveMsg(
+                        reset("")
+                                .append(
+                                        darkGray("[")
+                                )
+                                .append(
+                                        darkRed("-")
+                                ).append(
+                                darkGray("] ")
+                        ).append(
+                                (MutableText) ((TranslatableText) ((GameMessageS2CPacket) packet).getMessage()).getArgs()[0]
+                        )
+                        ,
+                        this.player
+                );
+            }
+            ci.cancel();
+        }
+    }
+
+    /**
+     * Determines if message packet contains
+     * a join/leave message
+     *
+     * @param packet
+     * @return if packet is a join or leave message
+     */
+    private boolean isJoinLeaveMessage(GameMessageS2CPacket packet) {
+        if (packet.getLocation().equals(MessageType.CHAT)) return false;
+        if (packet.getMessage() instanceof TranslatableText) {
+            TranslatableText message = (TranslatableText) packet.getMessage();
+            String key = message.getKey();
+            return key.equals("multiplayer.player.joined") || key.equals("multiplayer.player.left");
+        }
+
+        return false;
+    }
+
+    private boolean isJoinMessage(GameMessageS2CPacket packet) {
+        if (packet.getLocation().equals(MessageType.CHAT)) return false;
+        if (packet.getMessage() instanceof TranslatableText) {
+            TranslatableText message = (TranslatableText) packet.getMessage();
+            String key = message.getKey();
+            return key.equals("multiplayer.player.joined");
+        }
+
+        return false;
+    }
+
+    private boolean isLeaveMessage(GameMessageS2CPacket packet) {
+        if (packet.getLocation().equals(MessageType.CHAT)) return false;
+        if (packet.getMessage() instanceof TranslatableText) {
+            TranslatableText message = (TranslatableText) packet.getMessage();
+            String key = message.getKey();
+            return key.equals("multiplayer.player.left");
+        }
+
+        return false;
+    }
+
 }
--- src/main/kotlin/me/gserv/fabrikommander/utils/Loggers.kt
+++ src/main/kotlin/me/gserv/fabrikommander/utils/Loggers.kt
@@ -5,12 +5,16 @@ import org.apache.logging.log4j.Logger
 
 
 val CHAT_LOGGER = LogManager.getLogger("Chat") as Logger
+val JOIN_CHAT_LOGGER = LogManager.getLogger("Join") as Logger
+val LEAVE_CHAT_LOGGER = LogManager.getLogger("Leave") as Logger
 val STAFF_CHAT_LOGGER = LogManager.getLogger("Staff Chat") as Logger
 val PRIVATE_CHAT_LOGGER = LogManager.getLogger("Private Message") as Logger
 val RANK_LOGGER = LogManager.getLogger("Rank Manager") as Logger
 
 val loggers = hashMapOf(
     "chat.main" to CHAT_LOGGER,
+    "chat.join" to JOIN_CHAT_LOGGER,
+    "chat.leave" to LEAVE_CHAT_LOGGER,
     "chat.staff" to STAFF_CHAT_LOGGER,
     "chat.private" to PRIVATE_CHAT_LOGGER,
     "rank.main" to RANK_LOGGER
