1e90004e00a16d98ceb2c93e1c8922cb48acbc48
SpaceClouds42 <spaceclouds42@gmail.com>
2021-01-22 06:08:38 -0500

    update getDisplayName override to completely overwrite any PlayerEntity.getDisplayName() invocation (even in other mods :D )

---
+++ src/main/java/me/gserv/fabrikommander/mixin/PlayerEntityMixin_OverwriteDisplayName.java
@@ -0,0 +1,20 @@
+package me.gserv.fabrikommander.mixin;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+import static me.gserv.fabrikommander.extension.PlayerEntityKt.nickName;
+
+@Mixin(PlayerEntity.class)
+public class PlayerEntityMixin_OverwriteDisplayName {
+    /**
+     * @author
+     */
+    @Overwrite
+    public Text getDisplayName() {
+        return nickName((PlayerEntity) (Object) this);
+    }
+}
---
+++ src/main/kotlin/me/gserv/fabrikommander/extension/PlayerEntity.kt
@@ -0,0 +1,53 @@
+package me.gserv.fabrikommander.extension
+
+import me.gserv.fabrikommander.data.PlayerDataManager
+import me.gserv.fabrikommander.utils.*
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.text.ClickEvent
+import net.minecraft.text.HoverEvent
+import net.minecraft.text.MutableText
+
+fun PlayerEntity.nickName(): MutableText {
+    val prefix = if (rankToPrefix[PlayerDataManager.getRank(this.uuid)]?.shallowCopy() == null) {
+        reset("")
+    } else {
+        rankToPrefix[PlayerDataManager.getRank(this.uuid)]!!.shallowCopy()
+    }
+    val name: MutableText
+    val nick = PlayerDataManager.getNick(this.uuid)
+    if (nick == null) {
+        name = reset("") +
+                hover(
+                    click(
+                        gray(this.entityName),
+                        ClickEvent(
+                            ClickEvent.Action.SUGGEST_COMMAND,
+                            "/tell ${this.entityName}"
+                        )
+                    ),
+                    HoverEvent(
+                        HoverEvent.Action.SHOW_TEXT,
+                        white(this.entityName)
+                    )
+                ) +
+                reset("")
+    } else {
+        name = reset("") +
+                hover(
+                    click(
+                        reset(nick),
+                        ClickEvent(
+                            ClickEvent.Action.SUGGEST_COMMAND,
+                            "/tell ${this.entityName}"
+                        )
+                    ),
+                    HoverEvent(
+                        HoverEvent.Action.SHOW_TEXT,
+                        white(this.entityName)
+                    )
+                ) +
+                reset("")
+    }
+
+    return reset("") + prefix + name + reset("")
+}
