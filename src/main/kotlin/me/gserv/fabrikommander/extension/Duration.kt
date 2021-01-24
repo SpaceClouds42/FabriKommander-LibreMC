4c397c78df15fd081f2d8813557035c57ba60d1e
SpaceClouds42 <spaceclouds42@gmail.com>
2021-01-24 00:01:21 -0500

    better duration formatting

--- src/main/kotlin/me/gserv/fabrikommander/extension/Duration.kt
+++ src/main/kotlin/me/gserv/fabrikommander/extension/Duration.kt
@@ -3,11 +3,51 @@ package me.gserv.fabrikommander.extension
 import java.time.Duration
 
 fun Duration.prettyPrint(): String {
-    val fancy = this.toString()
+    val hmsArray = this.toString()
         .substring(2)
         .replace("(\\d[HMS])(?!$)".toRegex(), "$1 ")
         .replace("\\.\\d+".toRegex(), "")
         .toLowerCase()
-    //TODO: replace h, m, s with day(s), hour(s), minute(s), second(s)
-    return fancy
+        .split(" ")
+
+    var hours = 0
+    var minutes = 0
+    var seconds = 0
+
+    for (e in hmsArray) {
+        if (e.endsWith("h")) { hours   = e.replace("h", "").toInt() }
+        if (e.endsWith("m")) { minutes = e.replace("m", "").toInt() }
+        if (e.endsWith("s")) { seconds = e.replace("s", "").toInt() }
+    }
+
+    val days = hours/24
+    hours -= (days * 24)
+
+    var prettyString = ""
+
+    if (days == 1) {
+        prettyString += "1 day"
+    } else if (days > 0) {
+        prettyString += "$days days"
+    }
+
+    if (hours == 1) {
+        prettyString += " 1 hour"
+    } else if (hours > 0) {
+        prettyString += " $hours hours"
+    }
+
+    if (minutes == 1) {
+        prettyString += " 1 minute"
+    } else if (minutes > 0) {
+        prettyString += " $minutes minutes"
+    }
+
+    if (seconds == 1) {
+        prettyString += " 1 second"
+    } else if (seconds > 0) {
+        prettyString += " $seconds seconds"
+    }
+
+    return prettyString
 }
