5dbcf698113ca46fb12af4066f67be93f19eb592
SpaceClouds42 <spaceclouds42@gmail.com>
2021-01-23 19:49:18 -0500

    fix join messages not containing proper formatting

--- src/main/java/me/gserv/fabrikommander/mixin/PlayerEntityMixin_OverwriteDisplayName.java
+++ src/main/java/me/gserv/fabrikommander/mixin/PlayerEntityMixin_OverwriteDisplayName.java
@@ -1,19 +1,27 @@
 package me.gserv.fabrikommander.mixin;
 
+import com.mojang.authlib.GameProfile;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 
 import static me.gserv.fabrikommander.extension.PlayerEntityKt.nickName;
+import static me.gserv.fabrikommander.utils.RequestPlayerKt.requestPlayer;
 
 @Mixin(PlayerEntity.class)
 public class PlayerEntityMixin_OverwriteDisplayName {
+    @Shadow @Final private GameProfile gameProfile;
+
     /**
      * @author
      */
     @Overwrite
-    public Text getDisplayName() {
+    public Text getDisplayName() throws CommandSyntaxException {
+        requestPlayer(this.gameProfile);
         return nickName((PlayerEntity) (Object) this);
     }
 }
--- src/main/kotlin/me/gserv/fabrikommander/utils/RequestPlayer.kt
+++ src/main/kotlin/me/gserv/fabrikommander/utils/RequestPlayer.kt
@@ -5,12 +5,24 @@ import net.minecraft.server.network.ServerPlayerEntity
 import com.mojang.brigadier.exceptions.CommandSyntaxException
 import net.minecraft.server.command.ServerCommandSource
 import com.mojang.brigadier.context.CommandContext
+import me.gserv.fabrikommander.SERVER
 import me.gserv.fabrikommander.data.PlayerDataManager
 
-
+@Deprecated(
+    "CommandContext is no longer needed",
+    replaceWith = ReplaceWith(
+        "requestPlayer(requestedProfile)",
+        "import me.gserv.fabrikommander.utils.requestPlayer"
+    )
+)
 @Throws(CommandSyntaxException::class)
 fun requestPlayer(context: CommandContext<ServerCommandSource>, requestedProfile: GameProfile): ServerPlayerEntity {
-    val minecraftServer = context.source.minecraftServer
+    return requestPlayer(requestedProfile)
+}
+
+@Throws(CommandSyntaxException::class)
+fun requestPlayer(requestedProfile: GameProfile): ServerPlayerEntity {
+    val minecraftServer = SERVER
     var requestedPlayer = minecraftServer.playerManager.getPlayer(requestedProfile.name)
 
     if (requestedPlayer == null) {
